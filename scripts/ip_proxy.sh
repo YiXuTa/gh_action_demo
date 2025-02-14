251aca555a336ef7fcf4892960c920e38c763a85
xs <xushuai@promando.cn>
2025-02-14 13:34:25 +0800

    run ip_proxy2. tinyproxy

--- scripts/ip_proxy.sh
+++ scripts/ip_proxy.sh
@@ -4,6 +4,8 @@ date
 # 只有内网地址
 hostname -I
 pwd
+USERPWD=""
+
 
 curl ifconfig.me
 
@@ -12,8 +14,8 @@ tar xf $soft1.tar.xz
 ls -alh
 cd $soft1
 ./configure
-make
-make install
+echo $USERPWD | sudo -S make
+echo $USERPWD | sudo -S make install
 whereis tinyproxy
 echo "Done!"
 
